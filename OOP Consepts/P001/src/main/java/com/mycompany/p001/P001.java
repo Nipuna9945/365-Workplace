0fb1510b8095fb6a2cf5243300a9c9df3f680e51
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-03 14:28:36 +0530

    OOP File

---
+++ OOP Consepts/P001/src/main/java/com/mycompany/p001/P001.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.p001;
+
+/**
+ *
+ * @author acer
+ */
+public class P001 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+}
