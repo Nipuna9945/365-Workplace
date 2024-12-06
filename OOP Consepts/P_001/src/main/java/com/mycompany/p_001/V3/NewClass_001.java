555332f82faf690b855313ad29b653e5520e6fdd
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-06 23:04:37 +0530

    Get the sum value by using multiple  classes

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V3/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V3/NewClass_001.java
@@ -4,5 +4,7 @@ package com.mycompany.p_001.V3;
 
 public class NewClass_001 
 {
-    
+    int x = 5;
+    int y = 12;
+    int z = x + y;
 }
--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V3/NewClass_002.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V3/NewClass_002.java
@@ -1,13 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.p_001.V3;
 
-/**
- *
- * @author acer
- */
-public class NewClass_002 {
-    
+
+public class NewClass_002 
+{
+    public static void main(String[] args) 
+    {
+        NewClass_001 myObj1 = new NewClass_001();
+        NewClass_001 myObj2 = new NewClass_001();
+        NewClass_001 myObj3 = new NewClass_001();
+        
+        System.out.println("X value is : " + myObj1.x);
+        System.out.println("Y value is : " + myObj2.y);
+        System.out.println("Z value is : " + myObj2.z);
+    }
 }
