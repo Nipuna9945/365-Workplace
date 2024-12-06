c69b0c3c897fee5329e6c5fd79ed0548f58b21bc
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-06 22:33:33 +0530

    Multiple Classes
    Test the obj and classes in 2 different classes

---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V2/NewClass.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.p_001.V2;
+
+
+public class NewClass 
+{
+    public static void main(String[] args) 
+    {
+        P_001_V2 myObj = new P_001_V2();
+                
+        System.out.println(myObj.x);
+    }
+}
