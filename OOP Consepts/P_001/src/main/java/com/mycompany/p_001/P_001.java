bafacafc9bb962297e2333241ab4dd2e2be6d7eb
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-06 11:23:41 +0530

    New project file
    Remove the old files and replace them with new project file

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/P_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/P_001.java
@@ -2,20 +2,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.p001;
+package com.mycompany.p_001;
 
 /**
  *
  * @author acer
  */
-public class P001 
+public class P_001 
 {
     //int x = 5;
 
     public static void main(String[] args) 
     {
-        //P001 myObj = new P001();
+        System.out.println("Hello World!");
+        
+        //P_001 myObj = new P_001();
         //System.out.println(myObj.x);
-        System.err.println("Hello");
     }
 }
