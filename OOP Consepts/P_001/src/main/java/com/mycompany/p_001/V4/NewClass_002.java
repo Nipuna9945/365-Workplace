e4f2b81ca5df70c12e92bde3df71c2c931974fa0
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-07 13:34:00 +0530

    string and int values
    Code for all string and int values

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V4/NewClass_002.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V4/NewClass_002.java
@@ -1,13 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.p_001.V4;
 
-/**
- *
- * @author acer
- */
-public class NewClass_002 {
-    
+
+public class NewClass_002 
+{
+    public static void main(String[] args) 
+    {
+        NewClass_001 myObj1 = new NewClass_001();
+        NewClass_001 myObj2 = new NewClass_001();
+        NewClass_001 myObj3 = new NewClass_001();
+        
+        myObj3.x = 25;
+        
+        System.out.println("My full name is " + myObj1.N1 + " " + myObj2.N2 + " " + "and my age is " + myObj3.x);
+    }
 }
