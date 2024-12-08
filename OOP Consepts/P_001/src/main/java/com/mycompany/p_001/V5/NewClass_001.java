6a16f9900e6bcc251128a5b51d5cf6842cfea819
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-08 23:14:27 +0530

    Update NewClass_001.java
    Method in the class

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V5/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V5/NewClass_001.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.p_001.V5;
 
-/**
- *
- * @author acer
- */
-public class NewClass_001 {
-    
+
+public class NewClass_001 
+{
+    static void myMethod()
+    {
+        System.out.println("Hello world");
+    }
 }
