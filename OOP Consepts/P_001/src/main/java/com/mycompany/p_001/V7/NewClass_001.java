5d878a38077d88e28beb4f56d89a85feec30fc50
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-11 23:04:11 +0530

    Method
    Java method

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V7/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V7/NewClass_001.java
@@ -7,6 +7,10 @@ public class NewClass_001
     static void myMethod()
     {
         System.out.println("Hello world");
+    }
+    
+    public static void main(String[] args) 
+    {
         myMethod();
     }
 }
