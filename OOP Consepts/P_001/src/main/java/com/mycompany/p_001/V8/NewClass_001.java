dca4b8701ed0e846753e6f272f0a7ee2a1ccd766
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-13 16:51:05 +0530

    Update NewClass_001.java

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V8/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V8/NewClass_001.java
@@ -4,5 +4,19 @@ package com.mycompany.p_001.V8;
 
 public class NewClass_001 
 {
+    static void myStatic()
+    {
+        System.out.println("Hello this is the static one");
+    }
     
+    public void myPublic()
+    {
+        System.out.println("Hello this is the public one");
+    }
+    
+    
+    public static void main(String[] args) 
+    {
+        
+    }
 }
