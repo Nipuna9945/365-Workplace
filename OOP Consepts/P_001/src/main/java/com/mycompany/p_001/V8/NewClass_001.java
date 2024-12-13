1f30f724788a27d0b7efea897d2c35dbc4eae7a8
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-13 18:08:23 +0530

    Update NewClass_001.java
    Add comments to the code

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V8/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V8/NewClass_001.java
@@ -6,20 +6,20 @@ public class NewClass_001
 {
     static void myStaticMethod()
     {
-        System.out.println("Hello this is the static one");
+        System.out.println("Hello this is the static one");     // You dont need to make a object to call this static method
     }
     
     public void myPublicMethod()
     {
-        System.out.println("Hello this is the public one");
+        System.out.println("Hello this is the public one");     // You have to make a object to call this public method
     }
     
     
     public static void main(String[] args) 
     {
-        myStaticMethod();
+        myStaticMethod();       // Call the ststic methos
         
-        NewClass_001 myObj = new NewClass_001();
+        NewClass_001 myObj = new NewClass_001();        // Make an object and call the public method
         myObj.myPublicMethod();
     }
 }
