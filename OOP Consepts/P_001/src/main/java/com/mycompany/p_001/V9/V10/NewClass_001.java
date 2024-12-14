9cfdb0a148f58e0feee59a6288bb997ff1509b4b
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-14 11:37:03 +0530

    Separate classes
    Separate classes for previous example

---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V10/NewClass_001.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.p_001.V9.V10;
+
+public class NewClass_001 
+{
+    public void fullTrottle()
+    {
+        System.out.println("The bike is run faster as it can");
+    }
+    
+    public void Speed(int maxSpeed)
+    {
+        System.out.println("The bike speed is " + maxSpeed + "kmh-1");
+    }
+}
---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V10/NewClass_002.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.p_001.V9.V10;
+
+public class NewClass_002 
+{
+    public static void main(String[] args) 
+    {
+        NewClass_001 myObj = new NewClass_001();
+        myObj.fullTrottle();
+        
+        myObj.Speed(299);
+    }
+}
