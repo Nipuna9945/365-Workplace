e93195a77b8fa2d6d781775790ba3e66d95f1c74
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-14 23:37:39 +0530

    Update NewClass_001.java
    Update the code

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V11/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V11/NewClass_001.java
@@ -1,7 +1,14 @@
 
 package com.mycompany.p_001.V9.V11;
 
+import com.sun.tools.javac.Main;
+
 public class NewClass_001 
 {
     
+    
+    public static void main(String[] args) 
+    {
+        
+    }
 }
