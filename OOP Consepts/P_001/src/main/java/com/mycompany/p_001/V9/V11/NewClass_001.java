d5e7c255c3180ff1d85a93c578f9d3723e4cbce2
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-15 23:53:04 +0530

    constructor parameters
    java constructor with passing int value using constructor parameters

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V11/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V11/NewClass_001.java
@@ -6,7 +6,7 @@ public class NewClass_001
 {
     int x;
     
-    public NewClass_001()       // This is java constructor, make a method with the same name with the class. SO we dont need to call it will automatically run
+    public NewClass_001()       // This is ;;, make a method with the same name with the class. SO we dont need to call it will automatically run
     {
         x = 555;
     }
---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V12/NewClass_001.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.p_001.V9.V12;
+
+public class NewClass_001 
+{
+    int x;
+    
+    public NewClass_001(int y)
+    {
+        x = y;
+    }
+    
+    public static void main(String[] args) 
+    {
+        NewClass_001 myObj = new NewClass_001(526);
+        System.out.println(myObj.x);
+    }
+}
