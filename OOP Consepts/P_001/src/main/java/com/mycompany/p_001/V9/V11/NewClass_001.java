e4d94ea86aeb75f86906bed2da75eb06da57829a
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-15 23:29:43 +0530

    Update NewClass_001.java
    java constructor

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V11/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V11/NewClass_001.java
@@ -4,5 +4,17 @@ package com.mycompany.p_001.V9.V11;
 
 public class NewClass_001 
 {
+    int x;
     
+    public NewClass_001()
+    {
+        x = 555;
+    }
+    
+    public static void main(String[] args) 
+    {
+        NewClass_001 myObj = new NewClass_001();
+        
+        System.out.println(myObj.x);
+    }
 }
