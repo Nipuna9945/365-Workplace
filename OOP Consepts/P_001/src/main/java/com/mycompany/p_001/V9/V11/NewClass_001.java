af7175f5901da970bbfd03691665c3d15f8aebbb
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-15 23:30:02 +0530

    Update NewClass_001.java
    java constructor

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V11/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V11/NewClass_001.java
@@ -6,14 +6,14 @@ public class NewClass_001
 {
     int x;
     
-    public NewClass_001()
+    public NewClass_001()       // This is java constructor, make a method with the same name with the class. SO we dont need to call it will automatically run
     {
         x = 555;
     }
     
     public static void main(String[] args) 
     {
-        NewClass_001 myObj = new NewClass_001();
+        NewClass_001 myObj = new NewClass_001();        // make a obj and access the value
         
         System.out.println(myObj.x);
     }
