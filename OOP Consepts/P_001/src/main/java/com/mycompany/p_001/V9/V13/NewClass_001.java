8385c6aefa4e4fb79e72a5a436f80f4ad82f3891
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-16 21:41:32 +0530

    Update NewClass_001.java
    Constructor Parameters with int and string

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V13/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V13/NewClass_001.java
@@ -3,5 +3,19 @@ package com.mycompany.p_001.V9.V13;
 
 public class NewClass_001 
 {
+    int year;
+    String model;
     
+    public NewClass_001(int manifactureDate, String carModel)
+    {
+        year = manifactureDate;
+        model = carModel;
+    }
+    
+    public static void main(String[] args) 
+    {
+        NewClass_001 myObj = new NewClass_001(1986, "Mustang");
+        
+        System.out.println(myObj.year + " " + myObj.model);
+    }
 }
