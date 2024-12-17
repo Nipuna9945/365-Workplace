57ed88f8dfe244bce32cf230b679b0640921b4a3
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-17 23:32:40 +0530

    Update NewClass_001.java
    Protected Modifier

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V17/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V17/NewClass_001.java
@@ -1,7 +1,23 @@
 
 package com.mycompany.p_001.V9.V17;
 
-public class NewClass_001 
+class NewClass_002 
 {
+    protected String fName = "Nipuna";
+    protected String lName = "Randeer";
+    protected int age = 25;
+}
+
+class NewClass_001 extends NewClass_002 
+{
+    private int graduationYear = 2024;
     
+    public static void main(String[] args) 
+    {
+        NewClass_001 myObj = new NewClass_001();
+        
+        System.out.println("Full name is " + myObj.fName + " " + myObj.lName);
+        System.out.println("Age is " + myObj.age);
+        System.out.println("Graduation Year is " + myObj.graduationYear); 
+    }
 }
