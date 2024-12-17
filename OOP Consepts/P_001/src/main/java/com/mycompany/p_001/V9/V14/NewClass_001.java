1c0f6c98b4a38c187b56f786dcbc9268429985e2
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-17 21:26:40 +0530

    Java Modifiers

---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V14/NewClass_001.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.p_001.V9.V14;
+
+public class NewClass_001 
+{
+    public String fName = "Nipuna";
+    public String lName = "Randeer";
+    public int age = 25;
+    public String homeTown = "Gampaha";
+}
---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V14/NewClass_002.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.p_001.V9.V14;
+
+public class NewClass_002 
+{
+    public static void main(String[] args) 
+    {
+        NewClass_001 myObj = new NewClass_001();
+        
+    }
+}
