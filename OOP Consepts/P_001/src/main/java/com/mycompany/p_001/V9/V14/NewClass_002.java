4a410860060f956d78c4769af3aad690854722ee
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-17 22:09:33 +0530

    Update NewClass_002.java
    Access Modifiers
    public Modifier

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V14/NewClass_002.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V14/NewClass_002.java
@@ -6,6 +6,6 @@ public class NewClass_002
     public static void main(String[] args) 
     {
         NewClass_001 myObj = new NewClass_001();
-        
+        System.out.println("I`m " + myObj.fName + " " + myObj.lName + ". I`m " + myObj.age + ". Im from " + myObj.homeTown);
     }
 }
