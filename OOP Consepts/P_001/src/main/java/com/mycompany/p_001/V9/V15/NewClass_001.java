b7d376127f53dd724803d7ec8b2fd89620bbb2b6
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-17 22:29:34 +0530

    Create NewClass_001.java
    Private Modifier

---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V15/NewClass_001.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.p_001.V9.V15;
+
+public class NewClass_001 
+{
+    private String fName = "Nipuna";
+    private String lName = "Randeer";
+    private int age = 25;
+    private String homeTown = "Gampaha";
+    
+    
+    public static void main(String[] args) 
+    {
+       NewClass_001 myObj = new NewClass_001();
+       
+       System.out.println("I`m " + myObj.fName + " " + myObj.lName + ". I`m " + myObj.age + ". Im from " + myObj.homeTown);
+    }
+}
