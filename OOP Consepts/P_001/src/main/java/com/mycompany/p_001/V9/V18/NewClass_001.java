01ab46a98f70e57680d5899897f8e238ca88dc81
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-18 20:54:16 +0530

    Non-Access Modifiers
    Final Modifiers (attributes and methods)

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V18/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V18/NewClass_001.java
@@ -3,19 +3,5 @@ package com.mycompany.p_001.V9.V18;
 
 public class NewClass_001 
 {
-    final class Vehicle
-    {
-        protected String brand = "Ford";
-        
-        public void honk()
-        {
-            System.out.println("Peep, Peep");
-        }
-    }
     
-    class Main extends AbstractMethodError,
-    public static void main(String[] args) 
-    {
-        
-    }
 }
