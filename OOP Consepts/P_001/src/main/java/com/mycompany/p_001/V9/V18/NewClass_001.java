fba417ba050a8bb8982c2b4af730b53656bd7478
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-18 23:42:29 +0530

    Update NewClass_001.java
    final Modifier

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V18/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V18/NewClass_001.java
@@ -3,5 +3,16 @@ package com.mycompany.p_001.V9.V18;
 
 public class NewClass_001 
 {
+    final int x = 13;
+    final double Pi = 3.14;
     
+    public static void main(String[] args) 
+    {
+        NewClass_001 myObj = new NewClass_001();
+        
+        myObj.x = 50;       // will generate an error: cannot assign a value to a final variable
+        myObj.Pi = 2.49;        // will generate an error: cannot assign a value to a final variable
+                
+        System.out.println(myObj.x);
+    }
 }
