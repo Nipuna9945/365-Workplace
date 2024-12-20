99e2637e821f6ce945832a9bdcd8f2a360c970c3
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-20 23:17:47 +0530

    Update NewClass_001.java
    Public and static method

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V19/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V19/NewClass_001.java
@@ -3,11 +3,21 @@ package com.mycompany.p_001.V9.V19;
 
 public class NewClass_001 
 {
+    static void myStaticMethod()
+    {
+        System.out.println("This is my static method");
+    }
     
-    
+    public void myPublicMethiod()
+    {
+        System.out.println("This isn my public method");
+    }
     
     public static void main(String[] args) 
     {
+        myStaticMethod();
         
+        NewClass_001 myObj = new NewClass_001();
+        myObj.myPublicMethiod();
     }
 }
