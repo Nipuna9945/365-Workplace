0191867faf324091558734e6b52f98b784605539
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-21 23:36:56 +0530

    Abstract method and abstract class
    An abstract method belongs to an abstract class

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V20/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V20/NewClass_001.java
@@ -1,12 +1,20 @@
 
 package com.mycompany.p_001.V9.V20;
 
-public class NewClass_001 
+abstract class NewClass_001 
 {
+    public String Fname = "Nipuna";
+    public int age = 25;
     
+    public abstract void Study();
+}
+
+class Student extends NewClass_001
+{
+    public int graduationYear = 2025;
     
-    public static void main(String[] args) 
+    public void Study()
     {
-        
+        System.out.println("Studying all day long");
     }
 }
---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V20/NewClass_002.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.p_001.V9.V20;
+
+
+class NewClass_002 
+{
+    public static void main(String[] args) 
+    {
+        Student myObj = new Student();
+        
+        System.out.println("My first name is " + myObj.Fname);
+        System.out.println("My age is " + myObj.age);
+        System.out.println("My graduation year is " + myObj.graduationYear);
+        
+        myObj.Study();
+    }
+}
