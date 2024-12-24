54b8d2bdf24539dcb1e08af155aeeda5934068e4
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-24 23:42:09 +0530

    Update NewClass_001.java

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V21/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V21/NewClass_001.java
@@ -3,7 +3,8 @@ package com.mycompany.p_001.V9.V21;
 
 public class NewClass_001 
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         
     }
 }
