298f5840ba0e0599b0e480a76003b964a89b9161
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-26 23:27:27 +0530

    Encapsulation
    Getters and Setters

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V21/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V21/NewClass_001.java
@@ -3,5 +3,17 @@ package com.mycompany.p_001.V9.V21;
 
 public class NewClass_001 
 {
-    // Complete the rest of the code
+    private String name;
+
+    public String getName() 
+    {
+        return name;
+    }
+
+    public void setName(String name) 
+    {
+        this.name = name;
+    }
+    
+    
 }
