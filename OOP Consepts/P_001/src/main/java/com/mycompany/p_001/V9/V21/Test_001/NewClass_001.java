bb09b31e9d867d197b756072da3d2b591b09954f
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-27 00:00:55 +0530

    Get and set

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V21/Test_001/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V21/Test_001/NewClass_001.java
@@ -1,13 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.p_001.V9.V21.Test_001;
 
-/**
- *
- * @author acer
- */
-public class NewClass_001 {
+public class NewClass_001 
+{
+    private String Name;
+
+    public String getName() 
+    {
+        return Name;
+    }
+
+    public void setName(String Name) 
+    {
+        this.Name = Name;
+    }
+    
     
 }
--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V21/Test_001/NewClass_002.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V21/Test_001/NewClass_002.java
@@ -3,5 +3,16 @@ package com.mycompany.p_001.V9.V21.Test_001;
 
 public class NewClass_002 
 {
+    public static void main(String[] args) 
+    {
+        NewClass_001 myObj = new NewClass_001();
+        
+        myObj.setName("Nipuna");
+        
+        System.out.println(myObj.getName());
+    }
     
+    
+    
+   
 }
