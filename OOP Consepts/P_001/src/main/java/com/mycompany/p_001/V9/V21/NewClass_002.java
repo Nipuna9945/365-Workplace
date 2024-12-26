ba00d2fcd284a9e188296323c508de8b18870ea0
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-26 23:39:03 +0530

    Comment

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V21/NewClass_002.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V21/NewClass_002.java
@@ -3,5 +3,9 @@ package com.mycompany.p_001.V9.V21;
 
 public class NewClass_002 
 {
+    NewClass_001 myObj = new NewClass_001();
     
+    //myObj.name = "Nipuna"; // errors
+    
+    //System.out.println(myObj.name); // errors
 }
--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V21/Test_001/NewClass_002.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V21/Test_001/NewClass_002.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.p_001.V9.V21.Test_001;
 
-/**
- *
- * @author acer
- */
-public class NewClass_002 {
+public class NewClass_002 
+{
     
 }
