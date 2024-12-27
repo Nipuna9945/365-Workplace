225f27bee5d670672d413c2ed44a1461b72da0ca
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-27 19:35:57 +0530

    Update NewClass_001.java

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/Return_Type/Test_001/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/Return_Type/Test_001/NewClass_001.java
@@ -5,11 +5,13 @@ public class NewClass_001
 {
     static int myMethod(int x)
     {
-        return x = 531;
+        //return x = 531;
+        
+        return 531 + x;     // x value add to this 531 value
     }
     
     public static void main(String[] args) 
     {
-        System.out.println(myMethod(0));
+        System.out.println(myMethod(531));
     }
 }
