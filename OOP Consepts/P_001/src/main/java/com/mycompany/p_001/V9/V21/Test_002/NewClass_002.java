3a60d2fc4c0ad4c9efd41897a18f1c0f9d898976
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-28 12:29:47 +0530

    Encapsulation
    Encapsulation testing code

---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V21/Test_002/NewClass_002.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.p_001.V9.V21.Test_002;
+
+public class NewClass_002 
+{
+    public static void main(String[] args) 
+    {
+        NewClass_001 myObj = new NewClass_001();
+        
+        myObj.setfName("Nipuna");
+        myObj.setlName("Randeer");
+        myObj.setAge(25);
+        myObj.setHomeTown("Gampaha");
+        
+        System.out.println("Hey I`m " + myObj.getfName() + " " + myObj.getlName() + ". I`m " + myObj.getAge() + " years old. " + "I`m from " + myObj.getHomeTown() + ".");
+    }
+}
