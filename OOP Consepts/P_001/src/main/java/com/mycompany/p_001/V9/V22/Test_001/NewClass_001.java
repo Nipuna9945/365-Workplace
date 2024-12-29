5d179bffd2a42c65dd3d4450d7e9f0c0de517650
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-29 11:23:30 +0530

    Update NewClass_001.java

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V22/Test_001/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V22/Test_001/NewClass_001.java
@@ -5,12 +5,5 @@ import java.util.Scanner;
 
 public class NewClass_001 
 {
-    public static void main(String[] args) 
-    {
-        Scanner myObj = new Scanner(System.in);
-        
-        System.out.println("The word is ");
-    }
-    
     
 }
