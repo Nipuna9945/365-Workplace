4069954cc78f84da762c3b033f5d2a0d495683f1
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-29 13:36:09 +0530

    Test code
    Test the entire  code

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V22/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V22/NewClass_001.java
@@ -3,15 +3,13 @@ package com.mycompany.p_001.V9.V22;
 
 import java.util.Scanner;
 
-public class NewClass_001 
-{
-    public static void main(String[] args) 
-    {
+public class NewClass_001 {
+    public static void main(String[] args) {
+        
         Scanner myObj = new Scanner(System.in);
         
-        System.out.println("Enter a word : ");
+        System.out.println("Type a word : ");
         String myWord = myObj.nextLine();
-
-        System.out.println("Typed word is " + myWord);
+        System.out.println("The typed word is - " + myWord);
     }
 }
--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V22/Test_001/NewClass_001.java
+++
@@ -1,9 +0,0 @@
-
-package com.mycompany.p_001.V9.V22.Test_001;
-
-import java.util.Scanner;
-
-public class NewClass_001 
-{
-    
-}
