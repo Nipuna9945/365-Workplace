ef235eb36b08ad2f3690c9567430d6ac8c8a15e8
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-29 23:01:25 +0530

    Update NewClass_001.java
    String to int and int to string

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V23/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V23/NewClass_001.java
@@ -10,11 +10,14 @@ public class NewClass_001
         Scanner myObj = new Scanner(System.in);
         
         System.out.print("Enter your name : ");
-        System.out.print("Enter your age : ");
-        System.out.print("Enter your hometown : ");
-        
         String myWord1 = myObj.nextLine();
+        
+        System.out.print("Enter your age : ");
         int myNumber = myObj.nextInt();
+        
+        myObj.nextLine();
+        
+        System.out.print("Enter your hometown : ");
         String myWord2 = myObj.nextLine();
         
         System.out.println("Hello. My name is " + myWord1 + " and im " + myNumber + ". However im from " + myWord2);
