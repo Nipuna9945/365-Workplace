464d11701fa8171a26f937c50c4721ad58f2aca1
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-30 22:28:03 +0530

    Inheritance
    This is inheritance code

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V24/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V24/NewClass_001.java
@@ -1,11 +1,29 @@
 
 package com.mycompany.p_001.V9.V24;
 
+
+class Animal    // make parent class
+{
+    void eat()
+    {
+        System.out.println("Animal eat food");
+    }
+}
+
+class Dog extends Animal    // make child class and extends the child class from parent class01
+{
+    void barks()
+    {
+        System.out.println("Dog barks a lot");
+    }
+}
+
 public class NewClass_001
 {
-    
-    public static void main(String[] args)  
+    public static void main(String[] args)
     {
-        
+        Dog myObj = new Dog();      // make an object and then call it
+        myObj.eat();
+        myObj.barks();
     }
 }
