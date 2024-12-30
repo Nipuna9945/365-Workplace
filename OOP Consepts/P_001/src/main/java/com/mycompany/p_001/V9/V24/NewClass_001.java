5e7ceb54503f767d2205042d4f893632db10e746
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2024-12-30 18:42:08 +0530

    Create NewClass_001.java

---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V24/NewClass_001.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.p_001.V9.V24;
+
+/**
+ *
+ * @author acer
+ */
+public class NewClass_001
+{
+    
+}
