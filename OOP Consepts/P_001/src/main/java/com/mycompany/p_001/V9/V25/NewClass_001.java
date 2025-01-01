77840ba507577f3d6717c94ef15e1548aee40690
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-01 22:32:16 +0530

    Update NewClass_001.java
    Java Inheritance practice code

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V25/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V25/NewClass_001.java
@@ -1,12 +1,54 @@
 
 package com.mycompany.p_001.V9.V25;
 
+
+class Vehicle
+{
+    void drive()
+    {
+        System.out.println("Vehicle can drive");
+    }
+}
+
+class Car extends Vehicle
+{
+    void drift()
+    {
+        System.out.println("Car can drift");
+    }
+    
+    void reverse()
+    {
+        System.out.println("Car can reverse");
+    }
+}
+
+class Bike extends Vehicle
+{
+    void wheelie()
+    {
+        System.out.println("Bike can do a wheelie");
+    }
+    
+    void stunt()
+    {
+        System.out.println("Bike can do stunt");
+    }
+}
+
 public class NewClass_001
 {
-    
-    
     public static void main(String[] args)
     {
+        Car myCarObj = new Car();
+        Bike myBikeObj = new Bike();
+        
+        myCarObj.drive();
+        myCarObj.drift();
+        myCarObj.reverse();
+        
+        myBikeObj.wheelie();
+        myBikeObj.stunt();
         
     }
 }
