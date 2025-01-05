ee262efbf87c7be6ac827a4fd812674100d32930
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-05 23:24:24 +0530

    Update NewClass_001.java
    Java Inner Classes

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V26/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V26/NewClass_001.java
@@ -1,10 +1,22 @@
 
 package com.mycompany.p_001.V9.V26;
 
+
+class OuterClass
+{
+    int x = 26;
+}
+
+class InnerClass
+{
+    int y = 13;
+}
+
 public class NewClass_001
 {
     public static void main(String[] args)
     {
+        OuterClass myOutClass = new OuterClass();
         
     }
 }
