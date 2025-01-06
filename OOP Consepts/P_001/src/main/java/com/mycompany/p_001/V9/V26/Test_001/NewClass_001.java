0a88724944d9a14d46454c875c203973044f40b8
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-06 23:24:58 +0530

    Update NewClass_001.java
    Inner and outer classes with int value declaration

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V26/Test_001/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V26/Test_001/NewClass_001.java
@@ -1,7 +1,22 @@
 
 package com.mycompany.p_001.V9.V26.Test_001;
 
+
+class outerClass
+{
+    int x = 45;
+    
+    
+    class innerClass
+    {
+        int y = 98;
+    }
+}
+
 public class NewClass_001
 {
-    
+    public static void main(String[] args)
+    {
+        
+    }
 }
