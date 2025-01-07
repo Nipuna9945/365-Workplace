d6116aa438dc470d2223778b295e3ec939d51a22
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-07 20:20:17 +0530

    Update NewClass_001.java
    Static inner class

---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V26/Test_002/NewClass_001.java
@@ -0,0 +1,23 @@
+
+package com.mycompany.p_001.V9.V26.Test_002;
+
+
+
+class OuterClass
+{
+    int x = 57;
+    
+    static class innerClass
+    {
+        int y = 38;
+    }
+}
+
+public class NewClass_001
+{
+    public static void main(String[] args)
+    {
+        OuterClass.innerClass myInner = new OuterClass.innerClass();
+        System.err.println(myInner.y);
+    }
+}
--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V27/NewClass_001.java
+++
@@ -1,7 +0,0 @@
-
-package com.mycompany.p_001.V9.V27;
-
-public class NewClass_001
-{
-    
-}
