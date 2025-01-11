544837bbd3a1d5dcef40126304b1de12f903efee
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-11 23:31:29 +0530

    Update NewClass_001.java
    Whole code

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V27/Test_001/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V27/Test_001/NewClass_001.java
@@ -1,14 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.p_001.V9.V27.Test_001;
 
-/**
- *
- * @author acer
- */
+
+abstract class Vehicle
+{
+    public abstract void Drifting();
+    
+    public void Drive()
+    {
+        System.out.println("Put a gear and drive a vehicle");
+    }
+}
+
+class Car extends Vehicle
+{
+    public void Drifting()
+    {
+        System.out.println("Just press your accelarator and use handbreak to do a drift");
+    }
+}
+
 public class NewClass_001
 {
-    
+    public static void main(String[] args)
+    {
+        Car myObj = new Car();
+        myObj.Drive();
+        myObj.Drifting();
+    }
 }
