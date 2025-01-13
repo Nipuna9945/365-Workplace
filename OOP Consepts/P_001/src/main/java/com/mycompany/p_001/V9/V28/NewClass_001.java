1d4435d07acb75177342b83828a2cd44c22f6a4b
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-13 22:57:41 +0530

    Update NewClass_001.java

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V28/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V28/NewClass_001.java
@@ -1,6 +1,18 @@
 
 package com.mycompany.p_001.V9.V28;
 
+
+
+interface FirstInterface 
+{
+    
+}
+
+interface SecondInterface
+{
+    
+}
+
 public class NewClass_001
 {
     public static void main(String[] args)
