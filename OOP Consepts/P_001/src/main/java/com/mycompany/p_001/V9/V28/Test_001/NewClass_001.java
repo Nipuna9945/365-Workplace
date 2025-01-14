af4383c0a050c1662c5543fc9fe03148a70fd113
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-14 21:45:52 +0530

    Java interface
    Java interface with single interface and multiple interface

---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V28/Test_001/NewClass_001.java
@@ -0,0 +1,37 @@
+
+package com.mycompany.p_001.V9.V28.Test_001;
+
+interface FirstInterface 
+{
+    public void myMethod_001();
+}
+
+interface SecondInterface
+{
+    public void myMethod_001();
+}
+
+
+class DemoClass implements FirstInterface, SecondInterface
+{
+    public void myMethod_001()
+    {
+        System.out.println("Hello world");
+    }
+    
+    public void myMethod_002()
+    {
+        System.out.println("Chase dreams");
+    }
+}
+
+public class NewClass_001
+{
+    public static void main(String[] args)
+    {
+        DemoClass myObj = new DemoClass();
+        
+        myObj.myMethod_001();
+        myObj.myMethod_002();
+    }
+}
