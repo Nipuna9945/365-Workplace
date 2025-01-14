867e67174101c814c73444f3bd048a103d022835
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-14 22:39:09 +0530

    Update NewClass_001.java

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V28/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V28/NewClass_001.java
@@ -1,8 +1,34 @@
 
 package com.mycompany.p_001.V9.V28;
 
+
+interface Vehicle
+{
+    public void DrivingSound();
+    public void Drifting();
+}
+
+class Car implements Vehicle
+{
+    public void DrivingSound()
+    {
+        System.out.println("Turbo sound = Sutuzuuuuuuu");
+    }
+    
+    public void Drifting()
+    {
+        System.out.println("Press the gas and give em a show");
+    }
+}
+
 public class NewClass_001
 {
-    
+    public static void main(String[] args)
+    {
+        Car myObj = new Car();
+        
+        myObj.DrivingSound();
+        myObj.Drifting();
+    }
 }
 
