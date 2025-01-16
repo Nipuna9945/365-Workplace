f7a0977ef0169c5f73a23d67f3ef407d2d4bc74d
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-16 14:59:49 +0530

    enum
    enum with switch case

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V29/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V29/NewClass_001.java
@@ -3,5 +3,33 @@ package com.mycompany.p_001.V9.V29;
 
 public class NewClass_001
 {
+    enum Level
+    {
+        HIGH,
+        MEDIUM,
+        LOW
+    }
     
+    public static void main(String[] args)
+    {
+        Level myLevel = Level.HIGH;
+        
+        switch (myLevel)
+        {
+            case HIGH:
+                System.out.println("This level is high");
+                break;
+            
+            case MEDIUM:
+                System.out.println("This level is medium");
+                break;
+            
+            case LOW:
+                System.out.println("This level is low");
+                break;
+                
+            default:
+                throw new AssertionError();
+        }
+    }
 }
