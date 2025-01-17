3e8763d37bd30b6402d3a3f65d0309c75cb06c4d
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-17 16:19:40 +0530

    DateTimeFormatter

---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V32/NewClass_004.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.p_001.V9.V32;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class NewClass_004
+{
+    public static void main(String[] args)
+    {
+        LocalDateTime myObj = LocalDateTime.now();
+        System.out.println("Date and time before : " + myObj);
+        
+        DateTimeFormatter myFormatterObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String formatedDate = myObj.format(myFormatterObj);
+        System.err.println("After the format : " + formatedDate);
+    }
+}
