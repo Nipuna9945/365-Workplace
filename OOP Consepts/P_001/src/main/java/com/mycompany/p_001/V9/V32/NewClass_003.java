96f54e32c84583582498217a2c53d7756ecdf2dd
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-17 15:50:18 +0530

    DateTime

---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V32/NewClass_003.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.p_001.V9.V32;
+
+import java.time.LocalDateTime;
+
+public class NewClass_003
+{
+    public static void main(String[] args)
+    {
+        LocalDateTime myObj = LocalDateTime.now();
+        System.out.println("Today date and time is : " + myObj);
+    }
+}
