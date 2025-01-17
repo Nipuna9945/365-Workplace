93075a9e87c1dd394ae455b563d6bdc287bca57f
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-17 15:48:10 +0530

    Date and time

---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V32/NewClass_001.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.p_001.V9.V32;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class NewClass_001
+{
+    public static void main(String[] args)
+    {
+        LocalDate myObj = LocalDate.now();
+        System.out.println("Today is " + myObj);
+    }
+}
---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V32/NewClass_002.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.p_001.V9.V32;
+
+import java.time.LocalTime;
+
+public class NewClass_002
+{
+    public static void main(String[] args)
+    {
+        LocalTime myObj = LocalTime.now();
+        System.out.println("Time is : " + myObj);
+    }
+}
