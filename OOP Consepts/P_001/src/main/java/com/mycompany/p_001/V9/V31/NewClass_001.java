5c21549f3acfe66477c8c83515bc0ba091ee0130
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-17 15:34:50 +0530

    Create NewClass_001.java

---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V31/NewClass_001.java
@@ -0,0 +1,22 @@
+
+package com.mycompany.p_001.V9.V31;
+
+import java.util.Scanner;
+
+public class NewClass_001
+{
+    public static void main(String[] args)
+    {
+        Scanner myObj = new Scanner(System.in);
+        
+        System.out.println("Enter name, age, salary : ");
+        
+        String name = myObj.nextLine();
+        int age = myObj.nextInt();
+        double salary = myObj.nextDouble();
+        
+        System.out.println("Name is " + name);
+        System.out.println("Age is " + age);
+        System.out.println("Salary is " + salary);
+    }
+}
