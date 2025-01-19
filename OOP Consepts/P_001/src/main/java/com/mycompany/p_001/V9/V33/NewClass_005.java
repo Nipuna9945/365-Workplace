4160ed98682c783ebbbfb4cff86e88b480ef0fae
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-19 23:50:51 +0530

    Clear the array

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V33/NewClass_005.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V33/NewClass_005.java
@@ -3,7 +3,7 @@ package com.mycompany.p_001.V9.V33;
 
 import java.util.ArrayList;
 
-public class NewClass_004
+public class NewClass_005
 {
     public static void main(String[] args)
     {
@@ -14,7 +14,7 @@ public class NewClass_004
         cars.add("AMG");
         cars.add("GMC");
         
-        cars.clear();
+        
         
         System.out.println(cars);
     }
