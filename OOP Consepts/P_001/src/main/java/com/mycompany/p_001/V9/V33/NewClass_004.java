4bd559d7491737c1af2418de44e34d6d49f07e11
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-19 22:53:22 +0530

    Update NewClass_004.java
    Remove keyword
    \

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V33/NewClass_004.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V33/NewClass_004.java
@@ -1,7 +1,21 @@
 
 package com.mycompany.p_001.V9.V33;
 
+import java.util.ArrayList;
+
 public class NewClass_004
 {
-    
+    public static void main(String[] args)
+    {
+        ArrayList<String> cars = new ArrayList<>();
+        
+        cars.add("GT3RS");
+        cars.add("BMW");
+        cars.add("AMG");
+        cars.add("GMC");
+        
+        cars.remove(0);
+        
+        System.out.println(cars);
+    }
 }
