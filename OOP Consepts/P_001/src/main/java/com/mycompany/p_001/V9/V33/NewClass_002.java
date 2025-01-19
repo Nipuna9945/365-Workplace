e1415b97e1da3b7d9ca50e65712b7e71b4ef65dd
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-19 14:03:10 +0530

    Create NewClass_002.java

---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V33/NewClass_002.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.p_001.V9.V33;
+
+import java.util.ArrayList;
+
+public class NewClass_002
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> cars = new ArrayList<>();
+        
+        cars.add("BMW");
+        cars.add("AMG");
+        cars.add("GMC");
+        
+        cars.add(0, "GT3RS");
+        
+        System.out.println(cars);
+    }
+}
