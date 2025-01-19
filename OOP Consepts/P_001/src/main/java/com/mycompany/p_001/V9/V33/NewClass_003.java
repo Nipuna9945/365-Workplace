3364f3bb0eea0a0d6853c16f6a741c506a457e42
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-19 20:50:13 +0530

    Set value to the array

---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V33/NewClass_003.java
@@ -0,0 +1,21 @@
+
+package com.mycompany.p_001.V9.V33;
+
+import java.util.ArrayList;
+
+public class NewClass_003
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> cars = new ArrayList<>();
+        
+        cars.add("GT3RS");
+        cars.add("BMW");
+        cars.add("AMG");
+        cars.add("GMC");
+        
+        cars.set(0, "Mustang");
+        
+        System.out.println(cars);
+    }
+}
