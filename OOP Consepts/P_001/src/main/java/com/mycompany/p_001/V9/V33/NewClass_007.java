460065ffad77161496b6d2015f22a32732d553f8
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-20 23:32:41 +0530

    Create NewClass_007.java
     for-each

---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V33/NewClass_007.java
@@ -0,0 +1,22 @@
+
+package com.mycompany.p_001.V9.V33;
+
+import java.util.ArrayList;
+
+public class NewClass_007
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> cars = new ArrayList<>();
+        
+        cars.add("GT3RS");
+        cars.add("BMW");
+        cars.add("AMG");
+        cars.add("GMC");
+        
+        for(String i : cars)
+        {
+            System.out.println(i);
+        }
+    }
+}
