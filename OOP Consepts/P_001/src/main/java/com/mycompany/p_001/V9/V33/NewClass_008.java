4bc017fded634fec326e7e05ab1d84e60cabbfe2
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-21 00:11:39 +0530

    Create NewClass_008.java
    int ArrayList

---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V33/NewClass_008.java
@@ -0,0 +1,22 @@
+
+package com.mycompany.p_001.V9.V33;
+
+import java.util.ArrayList;
+
+public class NewClass_008
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        
+        numbers.add(18);
+        numbers.add(37);
+        numbers.add(26);
+        numbers.add(85);
+        
+        for(int i : numbers)
+        {
+            System.out.println(i);
+        }
+    }
+}
