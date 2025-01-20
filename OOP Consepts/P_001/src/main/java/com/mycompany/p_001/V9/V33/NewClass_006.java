14892f7cd395bd6ef313c36f73e47cbfe6342dd7
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-20 22:56:43 +0530

    Update NewClass_006.java
    for loop for read array and print it

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V33/NewClass_006.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V33/NewClass_006.java
@@ -1,10 +1,22 @@
 
 package com.mycompany.p_001.V9.V33;
 
+import java.util.ArrayList;
+
 public class NewClass_006
 {
     public static void main(String[] args)
     {
+        ArrayList<String> cars = new ArrayList<>();
         
+        cars.add("GT3RS");
+        cars.add("BMW");
+        cars.add("AMG");
+        cars.add("GMC");
+        
+        for (int i = 0; i < cars.size(); i++)
+        {
+            System.out.println(cars.get(i));
+        }
     }
 }
