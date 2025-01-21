6abe2e6e440d0129a4c83782b45bbc36ab19edca
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-21 22:28:43 +0530

    Update NewClass_010.java
    Number sotiog in an array

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V33/NewClass_010.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V33/NewClass_010.java
@@ -1,9 +1,27 @@
 
 package com.mycompany.p_001.V9.V33;
 
+import java.util.ArrayList;
+import java.util.Collections;
 
 
 public class NewClass_010
 {
-    
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        
+        numbers.add(46);
+        numbers.add(61);
+        numbers.add(94);
+        numbers.add(17);
+        numbers.add(24);
+        
+        Collections.sort(numbers);
+        
+        for (Integer i : numbers)
+        {
+            System.out.println(i);
+        }
+    }
 }
