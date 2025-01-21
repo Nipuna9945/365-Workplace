9ff8e6048804e398e65f458cfaa98e710f55091f
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-21 22:55:59 +0530

    Create NewClass_001.java
    LinkedList

---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V34/NewClass_001.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.p_001.V9.V34;
+
+import java.util.LinkedList;
+
+public class NewClass_001
+{
+    public static void main(String[] args)
+    {
+        LinkedList<String> cars = new LinkedList<>();
+        
+        cars.add("GT3RS");
+        cars.add("BMW");
+        cars.add("AMG");
+        cars.add("GMC");
+        
+        System.out.println(cars);
+    }
+}
