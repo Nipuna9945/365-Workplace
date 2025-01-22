a8aacd4e39495b3722fd90fd88f110dbef2cd61c
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-22 22:35:35 +0530

    Update NewClass_001.java
    Sort an ArrayList

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V35/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V35/NewClass_001.java
@@ -1,7 +1,25 @@
 
 package com.mycompany.p_001.V9.V35;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class NewClass_001
 {
-    
+    public static void main(String[] args)
+    {
+        ArrayList<String> cars = new ArrayList<>();
+        
+        cars.add("GT3RS");
+        cars.add("BMW");
+        cars.add("AMG");
+        cars.add("GMC");
+        
+        Collections.sort(cars);
+        
+        for (String i : cars)
+        {
+            System.out.println(i);
+        }
+    }
 }
