98f29e08b750dcc8dc4eb51fc73f195fed7aceed
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-22 22:40:58 +0530

    Update NewClass_002.java

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V35/NewClass_002.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V35/NewClass_002.java
@@ -1,11 +1,27 @@
 
 package com.mycompany.p_001.V9.V35;
 
-/**
- *
- * @author acer
- */
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class NewClass_002
 {
-    
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> number = new ArrayList<>();
+        
+        number.add(49);
+        number.add(14);
+        number.add(37);
+        number.add(88);
+        number.add(95);
+        number.add(63);
+        
+        Collections.sort(number);
+        
+        for (Integer i : number)
+        {
+            System.out.println(i);
+        }
+    }
 }
