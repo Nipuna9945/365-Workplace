8f9c805502bdf8af3be1e414bc54bbe68c2eed8f
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-23 23:36:38 +0530

    Create NewClass_004.java
    To remove all items, use the clear(

---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V36/NewClass_004.java
@@ -0,0 +1,21 @@
+
+package com.mycompany.p_001.V9.V36;
+
+import java.util.HashMap;
+
+public class NewClass_004
+{
+    public static void main(String[] args)
+    {
+        HashMap<String, String> myObj = new HashMap<>();
+        
+        myObj.put("Sri Lankan", "Colombo");
+        myObj.put("Canada", "Toronto");
+        myObj.put("USA", "Miami");
+        myObj.put("Australia", "Melbone");
+        myObj.put("Norway", "Oslo");
+        
+        myObj.clear();
+        System.out.println(myObj);
+    }
+}
