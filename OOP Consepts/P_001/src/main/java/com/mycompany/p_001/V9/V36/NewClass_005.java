1880768ceff2fd64bad0a4d8af44d03d431b354e
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-24 12:16:43 +0530

    Create NewClass_005.java
    To find out how many items there are, use the size() method:

---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V36/NewClass_005.java
@@ -0,0 +1,21 @@
+
+package com.mycompany.p_001.V9.V36;
+
+import java.util.HashMap;
+
+public class NewClass_005
+{
+    public static void main(String[] args)
+    {
+        HashMap<String, String> myObj = new HashMap<>();
+        
+        myObj.put("Sri Lankan", "Colombo");
+        myObj.put("Canada", "Toronto");
+        myObj.put("USA", "Miami");
+        myObj.put("Australia", "Melbone");
+        myObj.put("Norway", "Oslo");
+        
+        System.out.println(myObj.size());
+        
+    }
+}
