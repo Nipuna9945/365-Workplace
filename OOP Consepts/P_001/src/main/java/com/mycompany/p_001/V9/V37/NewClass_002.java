08b0b70895daf4c2acd8b8c1b91dc90d9ea46eb6
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-25 23:53:10 +0530

    myObj.contains

---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V37/NewClass_002.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.p_001.V9.V37;
+
+import java.util.HashSet;
+
+public class NewClass_002
+{
+    public static void main(String[] args)
+    {
+        HashSet<String> myObj = new HashSet<>();
+        
+        myObj.add("Volvo");
+        myObj.add("BMW");
+        myObj.add("Ford");
+        myObj.add("BMW");
+        myObj.add("Mazda");
+        
+        System.out.println(myObj.contains("Ford"));
+    }
+}
