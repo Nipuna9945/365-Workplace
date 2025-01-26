4c69d51c27b90adc2144e133af5eae2c9e83420a
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-27 00:15:56 +0530

    Update NewClass_007.java

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V37/NewClass_007.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V37/NewClass_007.java
@@ -1,14 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.p_001.V9.V37;
 
-/**
- *
- * @author acer
- */
+import java.util.HashSet;
+
 public class NewClass_007
 {
-    
+    public static void main(String[] args)
+    {
+        HashSet<Integer> myObj = new HashSet<>();
+        
+        myObj.add(6);
+        myObj.add(3);
+        myObj.add(9);
+        
+        for (int i = 1; i <= 10; i++)
+        {
+            if (myObj.contains(i))
+            {
+                System.out.println(i + "was found in the set");
+            } 
+            
+            else
+            {
+                System.out.println(i + "was not found in the set");
+            }
+        } 
+    }
 }
