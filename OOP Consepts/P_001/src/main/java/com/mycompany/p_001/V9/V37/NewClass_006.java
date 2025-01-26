1be85341608dc2e1591daab6cc06166853f9f2ab
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-26 22:08:19 +0530

    Create NewClass_006.java
    Forloop for HashSet

---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V37/NewClass_006.java
@@ -0,0 +1,23 @@
+
+package com.mycompany.p_001.V9.V37;
+
+import java.util.HashSet;
+
+public class NewClass_006
+{
+    public static void main(String[] args)
+    {
+        HashSet<String> myObj = new HashSet<>();
+        
+        myObj.add("Volvo");
+        myObj.add("BMW");
+        myObj.add("Ford");
+        myObj.add("BMW");
+        myObj.add("Mazda");
+        
+        for(String i : myObj)
+        {
+            System.out.println(i);
+        }
+    }
+}
