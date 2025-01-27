a2d91cbeb4120c306c3c38b409e128914a828365
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-27 23:24:02 +0530

    Update NewClass_001.java

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V38/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V38/NewClass_001.java
@@ -1,14 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.p_001.V9.V38;
 
-/**
- *
- * @author acer
- */
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class NewClass_001
 {
-    
+    public static void main(String[] args)
+    {
+        ArrayList<String> myObj = new ArrayList<>();
+        
+        myObj.add("Volvo");
+        myObj.add("BMW");
+        myObj.add("Ford");
+        myObj.add("BMW");
+        myObj.add("Mazda");
+        
+        Iterator<String> it = myObj.iterator();
+        
+        System.out.println(it);
+    }
 }
