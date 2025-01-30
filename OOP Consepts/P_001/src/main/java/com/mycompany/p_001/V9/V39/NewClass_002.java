3992333cc6d5a998f05595b62a497d03b0080b35
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-30 23:04:55 +0530

    Update NewClass_002.java
    To create a wrapper object, use the wrapper class instead of the primitive type. To get the value, you can just print the object

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V39/NewClass_002.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V39/NewClass_002.java
@@ -5,6 +5,12 @@ public class NewClass_002
 {
     public static void main(String[] args)
     {
+        Integer myInt = 46;
+        Double myDouble = 3.25;
+        Character myCharacter = 'A';
         
+        System.out.println(myInt);
+        System.out.println(myDouble);
+        System.out.println(myCharacter);
     }
 }
