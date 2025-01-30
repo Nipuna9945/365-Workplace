52ae9fd0468a2cba7a11756c50a885356f4f0a2f
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-30 23:10:20 +0530

    Create NewClass_003.java

---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V39/NewClass_003.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.p_001.V9.V39;
+
+public class NewClass_003
+{
+    public static void main(String[] args)
+    {
+        Integer myInt = 49;
+        Double myDouble = 4.18;
+        Character myCharactor = "Z"
+    }
+}
