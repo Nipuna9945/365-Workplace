586fffa4872d5be3426f94a33b7d5429269dbecb
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-30 22:59:30 +0530

    Update NewClass_001.java

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V39/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V39/NewClass_001.java
@@ -1,11 +1,22 @@
 
 package com.mycompany.p_001.V9.V39;
 
-/**
- *
- * @author acer
- */
+import java.util.ArrayList;
+
 public class NewClass_001
 {
-    
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> myObj = new ArrayList<>();
+        
+        myObj.add(10);
+        myObj.add(15);
+        myObj.add(20);
+        myObj.add(25);
+        
+        for (Integer i : myObj)
+        {
+            System.out.println(i);
+        }
+    }
 }
