4a94ddbde6c70744c7858e3726c98fd1ed1dbdee
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-30 15:33:39 +0530

    Update NewClass_003.java
    Iterator with remove int values

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V38/NewClass_003.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V38/NewClass_003.java
@@ -1,10 +1,31 @@
 
 package com.mycompany.p_001.V9.V38;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class NewClass_003
 {
     public static void main(String[] args)
     {
+        ArrayList<Integer> myObj = new ArrayList<>();
         
+        myObj.add(12);
+        myObj.add(8);
+        myObj.add(2);
+        myObj.add(23);
+        
+        Iterator<Integer> it = myObj.iterator();
+        
+        while (it.hasNext())
+        {
+            Integer i = it.next();
+            if (i < 10)
+            {
+                it.remove();
+            }
+        }
+        
+        System.out.println(myObj);
     }
 }
