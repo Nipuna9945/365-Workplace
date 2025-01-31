cd0d725b8735d7d8bd2ab0d843476a2815270ee2
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-31 23:05:00 +0530

    Java try and catch

---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V40/NewClass_001.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.p_001.V9.V40;
+
+public class NewClass_001
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            int[] myInt = {10, 27, 98};
+            System.out.println(myInt[27]);
+        } 
+        
+        catch (Exception e)
+        {
+            System.out.println("The number s not there");
+        }
+    }
+}
