2c6222a68cbb288076ff3cd2ee95ac2b54feea95
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-31 23:41:16 +0530

    Update NewClass_002.java
    Try,catch, finally

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V40/NewClass_002.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V40/NewClass_002.java
@@ -5,6 +5,20 @@ public class NewClass_002
 {
     public static void main(String[] args)
     {
+        try
+        {
+            int[] myint  = {10, 27, 98};
+            System.out.println(myint[32]);
+        } 
         
+        catch (Exception e)
+        {
+            System.out.println("Number is not there");
+        } 
+        
+        finally
+        {
+            System.out.println("Try catch is over");
+        }
     }
 }
