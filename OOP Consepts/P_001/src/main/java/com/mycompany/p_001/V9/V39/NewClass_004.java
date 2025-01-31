0e88a9b7d67f93159a20e7d6ba9bf86324939203
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-01-31 21:40:11 +0530

    myInt to String

---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V39/NewClass_004.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.p_001.V9.V39;
+
+public class NewClass_004
+{
+    public static void main(String[] args)
+    {
+        Integer myInt = 100;
+        String myString = myInt.toString();
+        
+        System.out.println(myString);
+        System.out.println(myString.length());
+    }
+}
