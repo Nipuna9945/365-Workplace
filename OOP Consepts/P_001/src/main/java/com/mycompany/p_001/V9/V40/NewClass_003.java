dbd9ff16cccc0c68a27c638ede9429a9a86f4793
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-01 21:59:30 +0530

    Update NewClass_003.java
    AbstractMethodError with throw

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V40/NewClass_003.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V40/NewClass_003.java
@@ -3,9 +3,21 @@ package com.mycompany.p_001.V9.V40;
 
 public class NewClass_003
 {
+    static void checkAge(int age)
+    {
+        if(age < 18)
+        {
+            throw new AbstractMethodError("Access denied  - You are old enough");
+        }
+        
+        else
+        {
+            System.out.println("Access granted - You are old enough");
+        }
+    }
     
     public static void main(String[] args)
     {
-        
+        checkAge(15);
     }
 }
