a86a2c62e97223d2bae4c6b8b8f4b1a70271f52a
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-02 22:25:55 +0530

    Update NewClass_001.java
    Pattern and Matcher

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V41/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V41/NewClass_001.java
@@ -1,14 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.p_001.V9.V41;
 
-/**
- *
- * @author acer
- */
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class NewClass_001
 {
-    
+    public static void main(String[] args)
+    {
+        Pattern pattern = Pattern.compile("boiz", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher("Hello boiz whats up");
+        
+    }
 }
