b19398fe869fb6aa49abb92a956391fc31887f18
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-03 23:24:24 +0530

    Running Threads

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V42/NewClass_002.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V42/NewClass_002.java
@@ -5,12 +5,15 @@ public class NewClass_002 implements Runnable
 {
     public static void main(String[] args)
     {
+        NewClass_002 obj = new NewClass_002();
+        Thread myTRD = new Thread(obj);
         
+        System.out.println("This code is outside of the thread");
     }
 
     @Override
     public void run()
     {
-        
+        System.out.println("This code is running in a thread");
     }
 }
