674d46668015ff06c150dd1868f1ae686f1ed347
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-03 23:21:12 +0530

    Runnable

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V42/NewClass_002.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V42/NewClass_002.java
@@ -1,14 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.p_001.V9.V42;
 
-/**
- *
- * @author acer
- */
-public class NewClass_002
+public class NewClass_002 implements Runnable
 {
-    
+    public static void main(String[] args)
+    {
+        
+    }
+
+    @Override
+    public void run()
+    {
+        
+    }
 }
