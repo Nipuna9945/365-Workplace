20105d819ad27416480d6e7ec5daeb779df50cd9
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-03 23:09:13 +0530

    Running Threads

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V42/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V42/NewClass_001.java
@@ -3,6 +3,14 @@ package com.mycompany.p_001.V9.V42;
 
 public class NewClass_001 extends Thread
 {
+    public static void main(String[] args)
+    {
+        NewClass_001 thread = new NewClass_001();
+        thread.start();
+        
+        System.out.println("This code is outside of the thread");
+    }
+    
     public void run()
     {
         System.out.println("This code is running in a thread");
