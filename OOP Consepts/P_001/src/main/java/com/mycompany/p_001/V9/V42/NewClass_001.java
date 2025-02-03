2846314cce42b0d804f779de4b9a9b51cbe004d4
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-03 22:35:20 +0530

    Update NewClass_001.java

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V42/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V42/NewClass_001.java
@@ -1,7 +1,10 @@
 
 package com.mycompany.p_001.V9.V42;
 
-public class NewClass_001
+public class NewClass_001 extends Thread
 {
-    
+    public void run()
+    {
+        System.out.println("This code is running in a thread");
+    }
 }
