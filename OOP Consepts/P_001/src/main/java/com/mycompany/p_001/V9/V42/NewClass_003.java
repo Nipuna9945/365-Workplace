69d709db9090cc187f719604348844ceff45ac1e
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-04 22:54:54 +0530

    isAlive()
    Thread with isAlive()

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V42/NewClass_003.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V42/NewClass_003.java
@@ -1,10 +1,22 @@
 
 package com.mycompany.p_001.V9.V42;
 
-public class NewClass_003
+public class NewClass_003 extends Thread
 {
+    public static int i = 0;
+    
     public static void main(String[] args)
     {
+        NewClass_003 myObj = new NewClass_003();
+        myObj.start();
         
+        while(myObj.isAlive())
+        {
+            System.out.println("Waiting...");
+        }
+        
+        System.out.println("Main: " + i);
+        i++;
+        System.out.println("Main: " + i);
     }
 }
