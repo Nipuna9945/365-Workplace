5185a6b0387364c251643fcc77c884fc49fe10aa
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-04 23:41:05 +0530

    Update NewClass_001.java

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V43/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V43/NewClass_001.java
@@ -12,7 +12,6 @@ public class NewClass_001
         numbers.add(9);
         numbers.add(8);
         numbers.add(1);
-        
         numbers.forEach((n) -> { System.out.println(n); } );
     }
 }
