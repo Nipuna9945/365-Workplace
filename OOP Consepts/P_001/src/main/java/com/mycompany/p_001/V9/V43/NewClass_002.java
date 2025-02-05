57212e38e1cecfdc25f613d23ed357c5bb7468c4
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-05 23:16:31 +0530

    Update NewClass_002.java
    Use Java's Consumer interface to store a lambda expression in a variable

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V43/NewClass_002.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V43/NewClass_002.java
@@ -1,7 +1,20 @@
 
 package com.mycompany.p_001.V9.V43;
 
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
 public class NewClass_002
 {
-    
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(5);
+        numbers.add(9);
+        numbers.add(8);
+        numbers.add(1);
+        Consumer<Integer> method = (n) -> {System.out.println(n); };
+        
+        numbers.forEach(method);
+    }
 }
