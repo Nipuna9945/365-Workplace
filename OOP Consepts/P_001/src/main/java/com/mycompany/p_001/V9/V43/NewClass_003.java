26d20f95668e0cfb14119e4c77df306c16b18f48
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-05 23:29:31 +0530

    Update NewClass_003.java
    Create a method which takes a lambda expression as a parameter

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V43/NewClass_003.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V43/NewClass_003.java
@@ -12,5 +12,13 @@ public class NewClass_003
     {
         StringFunction exclaim = (s) -> s + "!";
         StringFunction ask = (s) -> s + "?";
+        printFormatted("Hello", exclaim);
+        printFormatted("Hello", ask);
+    }
+    
+    public static void printFormatted(String str, StringFunction format) 
+    {
+        String result = format.run(str);
+        System.out.println(result);
     }
 }
