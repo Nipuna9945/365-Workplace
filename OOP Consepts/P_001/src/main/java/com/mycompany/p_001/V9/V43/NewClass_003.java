356b2e77a03fd196d29b56ff172c506c14cf19eb
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-05 23:28:34 +0530

    Update NewClass_003.java

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V43/NewClass_003.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V43/NewClass_003.java
@@ -1,10 +1,16 @@
 
 package com.mycompany.p_001.V9.V43;
 
+interface StringFunction 
+{
+  String run(String str);
+}
+
 public class NewClass_003
 {
     public static void main(String[] args)
     {
-        
+        StringFunction exclaim = (s) -> s + "!";
+        StringFunction ask = (s) -> s + "?";
     }
 }
