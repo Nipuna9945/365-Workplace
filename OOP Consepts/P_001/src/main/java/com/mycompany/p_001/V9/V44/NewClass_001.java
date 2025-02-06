528575b99f4bae9eceef0e151869e6fc1c72e409
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-06 23:20:35 +0530

    Update NewClass_001.java
    Create a comparator

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V44/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V44/NewClass_001.java
@@ -19,11 +19,20 @@ public class NewClass_001
         }
     }
     
-    class SortByYear implements Comparator {
-    public int compare(Object obj1, Object obj2) {
-    // Make sure that the objects are Car objects
-    Car a = (Car) obj1;
-    Car b = (Car) obj2;
+    class SortByYear implements Comparator 
+    {
+    public int compare(Object obj1, Object obj2) 
+    {
+        // Make sure that the objects are Car objects
+        Car a = (Car) obj1;
+        Car b = (Car) obj2;
+    
+        // Compare the year of both objects
+        if (a.year < b.year) return -1; // The first car has a smaller year
+        if (a.year > b.year) return 1;  // The first car has a larger year
+        return 0; // Both cars have the same year
+    }
+}
     
     
     
