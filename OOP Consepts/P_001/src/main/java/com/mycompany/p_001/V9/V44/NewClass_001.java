89fde9b2efccaa96fa72083e5baec310b9e90b96
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-06 22:57:09 +0530

    Define a Car class

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V44/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V44/NewClass_001.java
@@ -3,6 +3,13 @@ package com.mycompany.p_001.V9.V44;
 
 public class NewClass_001
 {
+    class Car
+    {
+        public String brand;
+        public String model;
+        public int year;
+    }
+    
     public static void main(String[] args)
     {
         
