785cacb6595895fa23a7626ff6d95c15c6425d34
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-06 23:04:43 +0530

    Update NewClass_001.java

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V44/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V44/NewClass_001.java
@@ -8,7 +8,17 @@ public class NewClass_001
         public String brand;
         public String model;
         public int year;
+        
+        public Car(String b, String m, int y)
+        {
+            brand = b;
+            model = m;
+            year = y;
+        }
+    
     }
+    
+    
     
     public static void main(String[] args)
     {
