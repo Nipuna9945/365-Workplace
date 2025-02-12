44eab7071eda76443c8f636bb56b2617bb1510df
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-12 23:35:57 +0530

    Create a file

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V45/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V45/NewClass_001.java
@@ -1,10 +1,29 @@
 
 package com.mycompany.p_001.V9.V45;
 
+import java.io.File;
+
 public class NewClass_001
 {
     public static void main(String[] args)
     {
         
+        try 
+        {
+            File myObj = new File("filename.txt");
+            if (myObj.createNewFile()) 
+            {
+                System.out.println("File created: " + myObj.getName());
+            } 
+            else 
+            {
+                System.out.println("File already exists.");
+            }
+        } 
+        catch (Exception e) 
+        {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
