88e638f459b4e6c71ec650d2ac0d9c01a303fddb
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-13 22:56:51 +0530

    Write in the java file

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V45/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V45/NewClass_001.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class NewClass_001
 {
     public static void main(String[] args)
-    {
+    { 
         
         try 
         {
--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V45/NewClass_002.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V45/NewClass_002.java
@@ -9,10 +9,15 @@ public class NewClass_002
     {
         try
         {
-            FileWriter myWriter = new FileWriter("filename.txt");
+            FileWriter myWriter = new FileWriter("E:\\365-Workplace\\OOP Consepts\\P_001\\src\\main\\java\\com\\mycompany\\p_001\\V9\\V45\\TextDocument.txt");
+            myWriter.write("Files in Java might be tricky, but it is fun enough");
+            myWriter.close();
+            System.out.println("Successfully wrote to the file");
         } 
         catch (Exception e)
         {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
     }
 }
