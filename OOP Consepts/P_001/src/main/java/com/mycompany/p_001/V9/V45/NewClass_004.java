011d6aa7aef98a7efc4687cf032e01bd1624e88f
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-14 00:00:26 +0530

    Update NewClass_004.java

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V45/NewClass_004.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V45/NewClass_004.java
@@ -8,5 +8,15 @@ public class NewClass_004
     public static void main(String[] args)
     {
         File myObj = new File("E:\\365-Workplace\\OOP Consepts\\P_001\\src\\main\\java\\com\\mycompany\\p_001\\V9\\V45\\DeleteDocument.txt");
+        
+        if(myObj.delete())
+        {
+            System.out.println("Deleted the file: " + myObj.getName());
+        }
+        
+        else
+        {
+            System.out.println("Failed to delete the file.");
+        }
     }
 }
