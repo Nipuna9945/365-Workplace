f67ea89289bb36cf37adeb83ad090b88b5c7ffaa
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-13 23:21:36 +0530

    Update NewClass_003.java

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V45/NewClass_003.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V45/NewClass_003.java
@@ -1,12 +1,19 @@
 
 package com.mycompany.p_001.V9.V45;
 
+import java.io.File;
 import java.io.FileWriter;
 
 public class NewClass_003
 {
     public static void main(String[] args)
     {
+        File myObj = new File("E:\\365-Workplace\\OOP Consepts\\P_001\\src\\main\\java\\com\\mycompany\\p_001\\V9\\V45\\TextDocument.txt");
         
+        if(myObj.exists())
+        {
+            System.out.println("File name: " + myObj.getName());
+            
+        }
     }
 }
