78b4ed29e6b7afa272c30ac15a4d757dd2682725
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-13 23:58:04 +0530

    Delete file in java

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V45/NewClass_004.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V45/NewClass_004.java
@@ -1,10 +1,12 @@
 
 package com.mycompany.p_001.V9.V45;
 
+import java.io.File;
+
 public class NewClass_004
 {
     public static void main(String[] args)
     {
-        
+        File myObj = new File("E:\\365-Workplace\\OOP Consepts\\P_001\\src\\main\\java\\com\\mycompany\\p_001\\V9\\V45\\DeleteDocument.txt");
     }
 }
