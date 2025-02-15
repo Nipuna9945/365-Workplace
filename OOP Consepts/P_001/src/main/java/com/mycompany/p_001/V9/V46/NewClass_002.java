755f9c188950e36360eec2975a65d485fee36417
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-15 23:12:07 +0530

    Create NewClass_002.java
    Add Two Numbers with User Input

---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V46/NewClass_002.java
@@ -0,0 +1,22 @@
+
+package com.mycompany.p_001.V9.V46;
+
+import java.util.Scanner;
+
+public class NewClass_002
+{
+    public static void main(String[] args)
+    {
+        int x, y, sum;
+        
+        Scanner myObj= new Scanner(System.in);
+        System.out.println("Type a number: ");
+        x = myObj.nextInt();
+        
+        System.out.println("Type another number: ");
+        y = myObj.nextInt();
+        
+        sum = x + y;
+        System.out.println("Sum is: " + sum);
+    }
+}
