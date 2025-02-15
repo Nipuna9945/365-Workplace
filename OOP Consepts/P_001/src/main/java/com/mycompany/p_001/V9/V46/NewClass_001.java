f81e2cc5098bef0beef467dd5fce473c906f5a6b
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-15 22:51:49 +0530

    Update NewClass_001.java

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V46/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V46/NewClass_001.java
@@ -1,17 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.p_001.V9.V46;
 
-/**
- *
- * @author acer
- */
 public class NewClass_001
 {
     public static void main(String[] args)
     {
+        int x = 15;
+        int y = 23;
         
+        int sum = x+ y;
+        
+        System.out.println(sum);
     }
 }
