52b5a77d63af204661412fa02e7bfc7bebcd4b31
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-21 00:18:51 +0530

    Update NewClass_001.java

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V48/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V48/NewClass_001.java
@@ -1,14 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.p_001.V9.V48;
 
-/**
- *
- * @author acer
- */
 public class NewClass_001
 {
-    
+    public static void main(String[] args)
+    {
+        for(int i = 0; i< OriginalSt.lengeth(): i ++)
+        {
+            
+        }
+    }
+ 
 }
