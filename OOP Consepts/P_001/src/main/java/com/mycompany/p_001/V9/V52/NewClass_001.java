38617fbec09662483c0fb833a19a894df949ffbf
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-23 23:29:18 +0530

    Create NewClass_001.java

---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V52/NewClass_001.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.p_001.V9.V52;
+
+public class NewClass_001
+{
+    public static void main(String[] args)
+    {
+        int ages[] = {20, 22, 18, 35, 48, 26, 87, 70};
+    }
+}
