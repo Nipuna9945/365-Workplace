b6d3dda4deb131dcd8deb5691ee012b956dcc733
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-23 23:15:17 +0530

    Create NewClass_001.java

---
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V51/NewClass_001.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.p_001.V9.V51;
+
+import java.util.Arrays;
+
+public class NewClass_001
+{
+    public static void main(String[] args)
+    {
+        String[] cars = {"Volvo", "BMW", "Tesla", "Ford", "Fiat", "Mazda", "Audi"};
+        Arrays.sort(cars);
+        
+        for (String i:cars)
+        {
+            System.out.println(i);
+        }
+    }
+}
