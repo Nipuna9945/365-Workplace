e25b499ef0daccc4586dbd7e07b33f63e6307795
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-24 23:12:51 +0530

    Update NewClass_001.java

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V55/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V55/NewClass_001.java
@@ -1,10 +1,21 @@
 
 package com.mycompany.p_001.V9.V55;
 
+import java.util.HashMap;
+
 public class NewClass_001
 {
     public static void main(String[] args)
     {
+        HashMap <String, String> capitalCities = new HashMap<>();
+        capitalCities.put("England", "London");
+        capitalCities.put("Germany", "Berlin");
+        capitalCities.put("Norway", "Oslo");
+        capitalCities.put("USA", "Washington DC");
         
+        for (String i : capitalCities.values())
+        {
+            System.out.println(i);
+        }
     }
 }
