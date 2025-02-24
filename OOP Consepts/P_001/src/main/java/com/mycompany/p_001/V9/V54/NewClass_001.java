5894243dcb8a3163c9a838d79cdf0d5818159bea
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-24 22:51:43 +0530

    Update NewClass_001.java

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V54/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V54/NewClass_001.java
@@ -1,11 +1,21 @@
 
 package com.mycompany.p_001.V9.V54;
 
-/**
- *
- * @author acer
- */
+import java.util.ArrayList;
+
 public class NewClass_001
 {
-    
+    public static void main(String[] args)
+    {
+        ArrayList<String>cars = new ArrayList<>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        
+        for (String i : cars)
+        {
+            System.out.println(i);
+        }
+    }
 }
