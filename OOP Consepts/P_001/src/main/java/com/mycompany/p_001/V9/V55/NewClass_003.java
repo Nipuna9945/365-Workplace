b55938a195886dbe2c9c73a4608f9a62344ae980
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-25 23:29:21 +0530

    Update NewClass_003.java

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V55/NewClass_003.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V55/NewClass_003.java
@@ -1,14 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.p_001.V9.V55;
 
-/**
- *
- * @author acer
- */
+import java.util.HashMap;
+
 public class NewClass_003
 {
+    public class Main 
+    {
+        public static void main(String[] args) 
+        {
+        
+            HashMap<String, String> capitalCities = new HashMap<String, String>();
+            capitalCities.put("England", "London");
+            capitalCities.put("Germany", "Berlin");
+            capitalCities.put("Norway", "Oslo");
+            capitalCities.put("USA", "Washington DC");
     
+            for (String i : capitalCities.keySet()) 
+            {
+                System.out.println("key: " + i + " value: " + capitalCities.get(i));
+            }
+        }
+    }
 }
