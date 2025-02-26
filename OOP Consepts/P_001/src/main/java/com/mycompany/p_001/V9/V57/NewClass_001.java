9aabd6dd7d6cbaa8aa76e9edd151a1efb9cd64ed
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-26 22:56:03 +0530

    Update NewClass_001.java

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V57/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V57/NewClass_001.java
@@ -1,14 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.p_001.V9.V57;
 
-/**
- *
- * @author acer
- */
 public class NewClass_001
 {
-    
+    public static void main(String[] args)
+    {
+        int length = 5;  
+        int width = 2;  
+        int area = length * width;  
+        System.out.println("Area of rectangle: " + area);  
+    }
 }
