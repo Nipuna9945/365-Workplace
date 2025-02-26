a324694cce7b4baec241c06c9394735366cf218b
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-26 22:59:20 +0530

    Update NewClass_001.java

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V58/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V58/NewClass_001.java
@@ -1,14 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.p_001.V9.V58;
 
-/**
- *
- * @author acer
- */
 public class NewClass_001
 {
+    public static void main(String[] args)
+    {
+        int number = 5;
     
+        if (number % 2 == 0) 
+        {
+            System.out.println(number + " is even.");
+        } 
+        else 
+        {
+            System.out.println(number + " is odd.");
+        }
+    }
 }
