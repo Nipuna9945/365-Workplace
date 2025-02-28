6e2f2c27555609efb62f6bd983d3f2bf25d52955
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-02-28 23:15:34 +0530

    Update NewClass_004.java

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V59/NewClass_004.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V59/NewClass_004.java
@@ -1,14 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.p_001.V9.V59;
 
-/**
- *
- * @author acer
- */
 public class NewClass_004
 {
-    
+    public static void main(String[] args)
+    {
+        int randomNum = (int)(Math.random() * 101);  // 0 to 100
+        System.out.println(randomNum);
+    }
 }
