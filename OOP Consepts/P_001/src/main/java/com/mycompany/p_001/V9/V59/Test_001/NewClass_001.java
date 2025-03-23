b012df31a0e5c48e4b7943f4892ccf95f12b7a9c
nipuna9945 <159068097+Nipuna9945@users.noreply.github.com>
2025-03-23 23:33:40 +0530

    Update NewClass_001.java

--- OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V59/Test_001/NewClass_001.java
+++ OOP Consepts/P_001/src/main/java/com/mycompany/p_001/V9/V59/Test_001/NewClass_001.java
@@ -7,4 +7,46 @@ public class NewClass_001
     private String lName;
     private int age;
     private String homeTown;
+
+    public String getfName()
+    {
+        return fName;
+    }
+
+    public void setfName(String fName)
+    {
+        this.fName = fName;
+    }
+
+    public String getlName()
+    {
+        return lName;
+    }
+
+    public void setlName(String lName)
+    {
+        this.lName = lName;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+
+    public String getHomeTown()
+    {
+        return homeTown;
+    }
+
+    public void setHomeTown(String homeTown)
+    {
+        this.homeTown = homeTown;
+    }
+    
+    
 }
